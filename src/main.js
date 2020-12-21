e59fc1aea2067929abe01fcb2e39af952bb237d7
yunwisdom <qq695926920@163.com>
2020-12-21 18:05:53 +0800

    init

--- src/main.js
+++ src/main.js
@@ -4,15 +4,12 @@ import VueExcelEditor from './VueExcelEditor.vue'
 import VueExcelColumn from './VueExcelColumn.vue'
 
 export default {
-  install (Vue) {
+    install(Vue) {
+        Vue.component('vue-excel-editor', VueExcelEditor)
+        Vue.component('vue-excel-column', VueExcelColumn)
 
-    Vue.component('vue-excel-editor', VueExcelEditor)
-    Vue.component('vue-excel-column', VueExcelColumn)
-
-    Vue.mixin({
-      created() {
-        // do something you like
-      }
-    })
-  }
+        Vue.mixin({
+            created() {}
+        })
+    }
 }
